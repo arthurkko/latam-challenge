9364456f76d0a0f40cb7c4b91bceee48e0f555d0
arthurkko <arthurkko@gmail.com>
2024-03-17 20:38:08 -0300

    doc: add comments

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -12,11 +12,14 @@ from memory_profiler import profile
 @profile
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
 
+    # Extract data
     df_q3 = extract_json(file_path=file_path, col=["mentionedUsers"])
 
+    # Make a list of users
     user_list = df_q3.mentionedUsers.tolist()
     del df_q3
 
+    # Dict: {username: count, ...}
     user_dict = {}
     for users in user_list:
         for user in users:
@@ -30,12 +33,15 @@ def q3_memory(file_path: str) -> List[Tuple[str, int]]:
     del user
     del username
     
+    # Dict -> Dataframe
     df_user = dict_to_df(user_dict, ["count"])
     del user_dict
 
+    # Top 10 in dataframe form
     df_q3_result = find_top10(df_user, "count", True)
     del df_user
 
+    # Top 10 List
     q3_result = df_to_list(df_q3_result, ["index", "count"])
     del df_q3_result
 
--- src/q3_time.py
+++ src/q3_time.py
@@ -9,11 +9,14 @@ from utils.dict_to_df import dict_to_df
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
     
+    # Extract data
     df_q3 = extract_json(file_path=file_path, col=["mentionedUsers"])
 
+    # Make a list of users
     user_list = df_q3.mentionedUsers.tolist()
     del df_q3
 
+    # Dict: {username: count, ...}
     user_dict = {}
     for users in user_list:
         for user in users:
@@ -27,12 +30,15 @@ def q3_time(file_path: str) -> List[Tuple[str, int]]:
     del user
     del username
     
+    # Dict -> Dataframe
     df_user = dict_to_df(user_dict, ["count"])
     del user_dict
 
+    # Top 10 in dataframe form
     df_q3_result = find_top10(df_user, "count", True)
     del df_user
 
+    # Top 10 List
     q3_result = df_to_list(df_q3_result, ["index", "count"])
     del df_q3_result
 
